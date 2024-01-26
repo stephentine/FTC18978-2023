9000aaa4705120848196eeab0bc060b1117b6971
stephentine <107305886+stephentine@users.noreply.github.com>
2024-01-26 15:48:29 -0500

    Update BasicTeleOp.java
    The latest TeleOp, with hanging arm controls and servo claw synced with intake

--- BasicTeleOp.java
+++ BasicTeleOp.java
@@ -14,9 +14,9 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class BasicTeleOp extends LinearOpMode {
     //Initialize motors, servos, sensors, imus, etc.
-    DcMotorEx m1,m2,m3,m4,intake,slide;
+    DcMotorEx m1,m2,m3,m4,intake,slide,plane,hanger;
     //DistanceSensor distance;
-    //Servo servo;
+    Servo arm,claw;
 
     public void runOpMode() {
 
@@ -28,11 +28,18 @@ public class BasicTeleOp extends LinearOpMode {
         m4 = (DcMotorEx) hardwareMap.dcMotor.get("back_right");
         intake = (DcMotorEx) hardwareMap.dcMotor.get("intake");
         slide = (DcMotorEx) hardwareMap.dcMotor.get("viper_slide");
-
+        plane = (DcMotorEx) hardwareMap.dcMotor.get("plane_launcher");
+        hanger = (DcMotorEx) hardwareMap.dcMotor.get("hanger");
+        arm = (Servo) hardwareMap.servo.get("drop_arm");
+        claw = (Servo) hardwareMap.servo.get("claw");
+        
         //Set them to the correct modes
         //This reverses the motor direction
-        m1.setDirection(DcMotorSimple.Direction.REVERSE);
-        m3.setDirection(DcMotorSimple.Direction.REVERSE);
+        m2.setDirection(DcMotorSimple.Direction.REVERSE);
+        m4.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Reverse the viper slide
+        slide.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //This resets the encoder values when the code is initialized
         m1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -59,6 +66,9 @@ public class BasicTeleOp extends LinearOpMode {
         // Set default intake toggle state
         double intakeState = 0.0;
         int bumperToggleState = 0;
+        
+        // Set time requirement for plane launcher
+        int planeButtonTimer = 0;
 
         waitForStart();
 
@@ -69,6 +79,7 @@ public class BasicTeleOp extends LinearOpMode {
             double pa = 0.0;
             if (gamepad1.dpad_down || gamepad1.dpad_up || gamepad1.dpad_left || gamepad1.dpad_right) {
                 // This allows for driving via dpad as well
+                // Uses ternaries to make the code more compact (condition ? if true : if false)
                 px = gamepad1.dpad_left ? -1.0 : 0.0;
                 px = gamepad1.dpad_right ? 1.0 : px;
                 py = gamepad1.dpad_down ? -1.0 : 0.0;
@@ -77,7 +88,13 @@ public class BasicTeleOp extends LinearOpMode {
                 // If the dpad is not in use, drive via sticks
                 px = gamepad1.left_stick_x;
                 py = -gamepad1.left_stick_y;
-                pa = -gamepad1.right_stick_x;
+                pa = gamepad1.right_stick_x;
+            }
+            
+            if (gamepad1.x) {
+                px *= 0.5;
+                py *= 0.5;
+                pa *= 0.5;
             }
             double p1 = px + py - pa;
             double p2 = -px + py + pa;
@@ -118,6 +135,7 @@ public class BasicTeleOp extends LinearOpMode {
                 if (bumperToggleState == 0) {
                     bumperToggleState = 1;
                 } else if (bumperToggleState == 1) {
+                    // Toggle intake on edge
                     if (intakeState == 0.0) {
                         intakeState = 1.0;
                     } else {
@@ -128,7 +146,72 @@ public class BasicTeleOp extends LinearOpMode {
             } else {
                 bumperToggleState = 0;
             }
-            intake.setPower(-intakeState);
+            
+            if (gamepad1.left_bumper) {
+                // Run the intake backwards
+                intake.setPower(1.0);
+            } else {
+                // Follow the toggle variable
+                intake.setPower(-intakeState);
+            }
+            
+            // Sync the claw with the intake
+            if (intakeState == 0.0) {
+                claw.setPosition(0.01);
+            } else {
+                claw.setPosition(0.15);
+            }
+    
+            // Viper Slide Manual Controls
+            slide.setPower(0.0);
+            if (gamepad2.y) {
+                slide.setPower(1.0);
+            } else if (gamepad2.a) {
+                slide.setPower(-1.0);
+            }
+
+            // Drop arm positioning
+            // 0.8 is 90 degree angle
+            // 0.44 is default position
+            if (gamepad2.right_trigger > 0.8) {
+                arm.setPosition(0.8);
+            } else if (gamepad2.right_trigger < 0.44) {
+                arm.setPosition(0.44);
+            } else {
+                arm.setPosition(gamepad2.right_trigger);
+            }
+            
+            // Plane launcher
+            if (gamepad2.left_trigger > 0.0) {
+                planeButtonTimer += 1;
+            } else {
+                planeButtonTimer = 0;
+            }
+            if (planeButtonTimer > 100) {
+                plane.setPower (1.0);
+            } else {
+                plane.setPower(0.0);
+            }
+            
+            
+            // Hanging arm
+            if (gamepad1.left_trigger > 0.8) { 
+                hanger.setPower(1.0);
+            } else if (gamepad1.right_trigger > 0.8) {
+                hanger.setPower(-1.0);
+            } else {
+                hanger.setPower(0.0);
+            }
+            
+            /*
+            if (gamepad1.left_trigger > 0.0) { 
+                hanger.setPower(gamepad1.left_trigger);
+            } else if (gamepad1.right_trigger > 0.0) {
+                hanger.setPower(-gamepad1.right_trigger);
+            } else {
+                hanger.setPower(0.0);
+            }
+            */
         }
     }
 }
